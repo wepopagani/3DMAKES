1e914ddfc48afc9d5923eab019ff382f2899b35c
marco pagani <wepo@MerkBook-Pro.local>
2025-02-23 11:10:12 +0100

    webapp 1.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,46 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: '3DMAKES',
+        short_name: '3DMAKES',
+        description: 'Servizi Professionali di Stampa 3D',
+        theme_color: '#ff0000',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true // Abilita la PWA anche durante lo sviluppo
+      }
+    }),
+  ],
   server: {
     headers: {
-      // Add Cross-Origin Embedder Policy header
       'Cross-Origin-Embedder-Policy': 'require-corp',
       'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Resource-Policy': 'cross-origin'
-    }
+      'Cross-Origin-Resource-Policy': 'cross-origin',
+    },
   },
   define: {
-    'process.env': process.env, // ✅ Qui leggiamo le variabili d'ambiente
-  }
+    'process.env': process.env,
+  },
 });
