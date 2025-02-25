cfa9d1e19812e70f82f4206f4fced8a04e65e463
marco pagani <wepo@MerkBook-Pro.local>
2025-02-25 11:59:46 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,22 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [
     react(),
-
   ],
   server: {
     headers: {
+      'Content-Security-Policy': `
+        default-src 'self';
+        script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.googleapis.com https://*.google.com;
+        style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+        img-src 'self' data: blob: https://*.google.com https://*.googleapis.com https://*.gstatic.com https://clients1.google.com;
+        font-src 'self' https://fonts.gstatic.com;
+        connect-src 'self' https://*.google.com https://*.googleapis.com https://prod.spline.design https://spline.design;
+        frame-src 'self' https://*.google.com;
+        worker-src 'self' blob:;
+      `.replace(/\s+/g, ' ').trim(),
       'Cross-Origin-Embedder-Policy': 'require-corp',
       'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Resource-Policy': 'cross-origin',
+      'Cross-Origin-Resource-Policy': 'cross-origin'
     },
   },
   define: {
