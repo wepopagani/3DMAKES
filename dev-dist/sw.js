c5afe91e32108e92f6edb43f1309795c00d08337
marco pagani <wepo@MerkBook-Pro.local>
2025-02-25 11:33:42 +0100

    photos

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.sdanisr1dqo"
+    "revision": "0.8o34ushk90o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
