5dabc38f670e625bc667781e3aa030a493dda430
marco pagani <wepo@MerkBook-Pro.local>
2025-02-25 11:58:24 +0100

    remove webapp

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: '3DMAKES',
-        short_name: '3DMAKES',
-        description: 'Servizi Professionali di Stampa 3D',
-        theme_color: '#ff0000',
-        background_color: '#ffffff',
-        display: 'standalone',
-        start_url: '/',
-        icons: [
-          {
-            src: '/icons/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/icons/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          }
-        ]
-      },
-      devOptions: {
-        enabled: true // Abilita la PWA anche durante lo sviluppo
-      }
-    }),
+
   ],
   server: {
     headers: {
