839a2b6f3106347ad823e70891614326a19ac62c
marco pagani <wepo@MerkBook-Pro.local>
2025-03-16 10:04:23 +0100

    userPanel 1.1

--- src/components/AdminPanel.tsx
+++ src/components/AdminPanel.tsx
@@ -1438,13 +1438,13 @@ const AdminPanel: React.FC = () => {
                               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
                             </svg>
                             <p className="text-sm text-gray-400">Carica un file o trascinalo qui</p>
-                            <p className="text-xs text-gray-500 mt-1">PNG, JPG, GIF, STL, OBJ, GLTF</p>
+                            <p className="text-xs text-gray-500 mt-1">Tutti i tipi di file supportati</p>
                             <input 
                               ref={fileInputRef}
                               type="file"
                               onChange={handleFileChange}
                               className="hidden"
-                              accept="image/*,.obj,.stl,.gltf"
+                              accept="*/*"
                             />
                           </div>
                           
