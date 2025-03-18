f030ff3d26d6e64f59259bfca487f67ae0023839
marco pagani <wepo@MerkBook-Pro.local>
2025-03-18 10:28:44 +0100

    non

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// Fix per useLayoutEffect in ambienti SSR/SSG
+if (typeof window === 'undefined') {
+  React.useLayoutEffect = React.useEffect;
+} else if (!React.useLayoutEffect) {
+  React.useLayoutEffect = React.useEffect;
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
